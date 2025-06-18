ebb7358281ebfdc4a6f916548d7ac0f072a3fc04
feng2323 <feng.tr@icloud.com>
2025-06-18 11:50:41 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
+
+app.post('/notify', async (req, res) => {
+  const message = req.body.message;
+  if (!message) {
+    return res.status(400).send('No message provided');
+  }
+  try {
+    await axios.post(slackWebhookUrl, { text: message });
+    res.status(200).send('Notification sent');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error sending notification');
+  }
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`App listening on port ${listener.address().port}`);
+});
